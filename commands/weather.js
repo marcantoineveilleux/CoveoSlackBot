0ca00cde0c519ff75ed7633ae192b8c98a7c78d5
Guillaume Simard <guisim@gmail.com>
2014-05-01 12:57:11 -0400

    Added !weather

---
+++ commands/weather.js
@@ -0,0 +1,23 @@
+var request = require("request");
+
+module.exports = {
+	def : {
+		exec : function (hook, callback) {
+			request("http://api.openweathermap.org/data/2.5/weather?q=" + hook.command_text, function (err, res, body) {
+				if (err || res.statusCode != 200) {
+					callback("Error " + err);
+				} else {
+                    data = JSON.parse(body);
+                    if (data.cod != "200") {
+						callback(data.message);
+					} else {
+                        callback((data.main.temp - 273).toFixed(2) + " Celsius, " + data.weather[0].main);
+                    }
+				}
+			});
+		},
+		help : function (callback) {
+			callback("Display the current weather somewhere. Usage !weather @city (e.g. !weather Jonquière)");
+		}
+	}
+}
