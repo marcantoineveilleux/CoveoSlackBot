99f2387e408a5fc679b7246b0dbba06117400268
Guillaume Simard <guisim@gmail.com>
2014-05-01 13:03:59 -0400

    Added city+country

--- commands/weather.js
+++ commands/weather.js
@@ -11,7 +11,7 @@ module.exports = {
                     if (data.cod != "200") {
 						callback(data.message);
 					} else {
-                        callback((data.main.temp - 273).toFixed(2) + " Celsius, " + data.weather[0].main);
+                        callback("Weather in " + data.name + ", " + data.sys.country + ": " + (data.main.temp - 273).toFixed(2) + " Celsius, " + data.weather[0].main);
                     }
 				}
 			});
