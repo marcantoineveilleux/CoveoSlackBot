86ae9ca84bebdfe9f61ca7942e57f4c61aef645e
apinc <alex.pare.inc@gmail.com>
2014-05-22 13:12:21 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -5,27 +5,30 @@ module.exports = {
 	def : {
 		exec : function (hook, callback) {
 			var commandArguments = hook.command_text.split("|");
-
-			getMeme(commandArguments[0].toLowerCase(), function(id, err){
-				if(err) {
-					callback(err);
-				} else {
-				  var formParams = {
-				    template_id: id,
-				    username: "coveoslackbot",
-				    password: "tamere",
-				    text0: commandArguments[1] + "",
-				    text1: commandArguments[2] + ""
-			      };
-			      request.post('https://api.imgflip.com/caption_image', function(e, r, body) {
-			      	var result = JSON.parse(body);
-			      	if(result.success == true) {
-			      	  callback(result.data.url);
-			      	} else {
-			          callback("Error with the meme provider.");
-			      	}
-			      }).form(formParams);
+			if(commandArguments.length > 1){
+				getMeme(commandArguments[0].toLowerCase(), function(id, err){
+					if(err) {
+						callback(err);
+					} else {
+					  var formParams = {
+					    template_id: id,
+					    username: "coveoslackbot",
+					    password: "tamere",
+					    text0: commandArguments[1] + "",
+					    text1: commandArguments[2] + ""
+				      };
+				      request.post('https://api.imgflip.com/caption_image', function(e, r, body) {
+				      	var result = JSON.parse(body);
+				      	if(result.success == true) {
+				      	  callback(result.data.url);
+				      	} else {
+				          callback("Error with the meme provider.");
+				      	}
+				      }).form(formParams);
 				}
+			} else {
+				getMemes(commandArguments[0].toLowerCase(), callback);
+			}
 			});
 
 			function getMeme(query, fnCallback) {
@@ -41,9 +44,23 @@ module.exports = {
 			      }     
 			    })
 			}
+
+			function getMemes(query, fnCallback) {
+			    request.get('https://api.imgflip.com/get_memes', function(e, r, body) {
+			      var memes = JSON.parse(body).data.memes;
+			      var matches = _.filter(memes, function(meme) {
+			        return meme.name.toLowerCase().indexOf(query) >= 0;
+			      });
+			      if(matches.length == 0) {
+			      	fnCallback("No meme found!");
+			      } else {
+			        fnCallback(matches.map(function(match){return match.id}).join(', '));
+			      }     
+			    })
+			}
 		},
 		help : function (callback) {
-			callback("Creates a meme. Usage !meme @queryForName|@text0|@text1");
+			callback("Creates a meme. Usage !meme @queryForName|@text0|@text1. Use !meme @queryForName to list meme");
 		}
 	}
 }
