9cfb20af1e362a6ccaba4e6153bac109f7f0d661
Guillaume Simard <guisim@gmail.com>
2014-08-13 10:27:48 -0400

    Added support for !ops command

---
+++ commands/ops.js
@@ -0,0 +1,19 @@
+﻿key = new Buffer("ZGY2MTBiY2ItY2E3NC00MGRhLTk5YzctM2VhZjY2ZmE1NWMy", "base64").toString("ascii")
+var postmark = require("postmark")(key);
+
+module.exports = {
+  def: {
+    exec : function (hook, callback) {
+        postmark.send({
+            "From": "coveoslackbot@coveo.com", 
+            "To": "cloudops@coveo.com", 
+            "Subject": "Request from " + hook.user_name, 
+            "TextBody": hook.command_text
+        });
+      callback("Sent email to cloudops@coveo.com.");
+    },
+    help : function(callback) {
+      callback("Send a mail to cloudops@coveo.com. Usage !ops @email_body");
+    }
+  }
+}
